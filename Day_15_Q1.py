ffe1bb9611ce763ab31156eaf737ee6713e04c9c
yashkumarjha <55932900+yashkumarjha@users.noreply.github.com>
2024-03-03 11:26:33 +0530

    Create Day_15_Q1.py

---
+++ Day_15_Q1.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        left = 0
+        right = 1
+        while(right < len(prices)):
+            if(prices[left]<prices[right]):
+                profit = prices[right]-prices[left]
+                max_profit = max(profit, max_profit)
+            else:
+                left=right
+            right = right+1
+        return max_profit
